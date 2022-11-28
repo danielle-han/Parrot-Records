67ca819e18d3c4a97a4320fc8ead79cea9b2ed18
danielle-han <61366948+danielle-han@users.noreply.github.com>
2022-11-27 16:30:24 -0800

    initial rest-api files added
    initialized project, added dependencies, added code to model file.

---
+++ parrots-restapi/parrot.test.js
@@ -0,0 +1,9 @@
+// import
+
+// to run tests: npm test or jest
+
+// unit tests: simple function that gets 2 parameters: 
+// 1st: string that describes the test
+// 2nd: another function that expresses the action that is tested
+
+// test every endpoint
---
+++ parrots-restapi/parrots_model.mjs
@@ -0,0 +1,63 @@
+import mongoose from "mongoose";
+import 'dotenv/config';
+
+// connects to MongoDB and uses the variable from the .env file as the url.
+mongoose.connect(
+    process.env.MONGODB_CONNECT_STRING,
+    { useNewUrlParser: true }
+);
+
+// connect to the database:
+const db = mongoose.connection;
+
+// open event is called when connection successfully opens 
+// remove?
+db.once("open", () => {
+    console.log("successfully connected to MongoDB");
+})
+
+// defining schema:
+// unique: true? - name?
+const parrotSchema = mongoose.Schema({
+    name: { type: String, required: true },
+    weight: { type: Number, required: true },
+    age_years: { type: Number, required: true },
+    age_months: { type: Number, required: true },
+    hatch_date: { type: Date, required: true},
+    species: { type: String, required: true}
+});
+
+// compile parrot model from the schema above
+const Parrot = mongoose.model("Parrot", parrotSchema);
+
+// create new parrot
+const createParrot = async (name, weight, age_years, age_months, hatch_date, species) => {
+    const parrot = new Parrot({name: name, weight: weight, age_years: age_years, age_months: age_months, hatch_date: hatch_date, species: species});
+    return parrot.save();
+}
+
+// retrieve parrots
+const findParrots = async () => {
+    const query = Parrot.find();
+    return query.exec();
+}
+
+// find parrot by ID
+const findParrotById = async (_id) => {
+    const query = Parrot.findById(_id);
+    return query.exec();
+}
+
+// edit parrot
+const editParrot = async (_id, name, weight, age_years, age_months, hatch_date, species) => {
+    const result = await Parrot.replaceOne({_id: _id}, {name: name, weight: weight, age_years: age_years, age_months: age_months, hatch_date: hatch_date, species: species});
+    return result.modifiedCount;
+}
+
+// delete parrot by ID
+const deleteById = async (_id_) => {
+    const result = await Parrot.deleteOne({_id: _id});
+    return result.deletedCount;
+};
+
+export {createParrot, findParrots, findParrotById, editParrot, deleteById}
